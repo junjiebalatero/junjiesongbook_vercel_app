d8ad173067c1220d7b99684eb8c59cff4282927f
junjiebalatero <junjiebalatero@gmail.com>
2023-12-28 18:15:34 +0800

    first commit

---
+++ app/survival/page.tsx
@@ -0,0 +1,84 @@
+import Link from 'next/link'
+export default function Page() {
+    return (
+        
+        // <Link href="/">this is dashboard click to go to home </Link>
+
+        <pre>{`
+    Survival
+
+        [Intro]
+    Am  Em  Am  Em  Fmaj7  Em  Fmaj7  Em
+    
+    [Verse]
+    Em         Am       Em           Am
+    I'll survive you,   I will survive you
+    Em            F           Em       F                 Em
+    Well, it's too late, I know, to change the way we go
+    Em           Am     Em           Am
+    From this day on    I want to stay on
+    G          F            Em
+    Now that I've started learning
+            F             Em
+    And the tides started turning
+            F              Esus4  Em
+    Well, it's here I want to stay
+    
+    [Chorus]
+    Dm7              G7     Em          Am
+        I'll let the moon announce my arrival
+    Dm7          G7       Em
+        To every eye that cares to see
+    F              G7     Em        Am
+    Just working on the art of survival
+    F                         Em
+    Where do we go from here, where do we go from here
+    
+    [Verse]
+    Em      Am        Em          Am
+    I'm alive here,   I can survive dear
+    G            F             Em
+    Though the weather might change
+            F           Em
+    From the wind to the rain
+            F                 Esus4  Em
+    Well, my heart remains the same
+    
+    [Chorus]
+    Dm7              G7     Em          Am
+        I'll let the moon announce my arrival
+    Dm7          G7       Em
+        To every eye that cares to see
+    F              G7     Em        Am
+    Just working on the art of survival
+    F                         Em
+    Where do we go from here, where do we go from here
+    
+    [Bridge]
+    C
+    Looked out on a sea of trouble
+    
+    And I tried to stall for awhile
+    Am                     D
+    It's so hard to answer every question
+            Am                   Fmaj7
+    And it's hard to protect your heart anymore
+    
+    [Interlude] Lead Break
+    Em  Am  Em  Am  G  F  Em  F  Em
+    
+    [Chorus] X3
+    Dm7              G7     Em          Am
+        I'll let the moon announce my arrival (I am alive)
+    Dm7          G7       Em
+        To every eye that cares to see
+    F              G7     Em        Am
+    Just working on the art of survival (but I wonder)
+    Fmaj7               Esus4  Em
+    Where do we go from here
+      `}</pre>
+
+        )
+          
+    
+  }
---
+++ app/takeiteasy/page.tsx
@@ -0,0 +1,103 @@
+import Link from 'next/link'
+export default function Page() {
+    return (
+        
+        // <Link href="/">this is dashboard click to go to home </Link>
+
+        <pre>{`
+     Take It Easy
+     
+    [Intro]
+    G       Am7/G  D7sus4/G  x2
+    G
+    
+    [Verse 1]
+                G
+    Well, I'm a-runnin' down the road tryin' to loosen my load
+                    D     C
+    I got seven women on my mind
+    G                       D
+    Four that wanna own me, two that wanna stone me
+    C                              G
+    One says she's a friend of mine
+    
+    [Chorus]
+            Em            C G
+    Take it easy, take it easy
+                Am                C                 Em
+    Don't let the sound of your own wheels drive you crazy
+            C                 G
+    Lighten up while you still can
+            C             G
+    Don't even try to understand
+                Am                  C                  G
+    Just find a place to make your stand, and take it easy
+    
+    [Verse 2]
+                G
+    Well, I'm a-standin' on a corner in Winslow, Arizona
+                D        C
+    Such a fine sight to see
+        G                   D
+    It's a girl, my Lord, in a flat-bed Ford
+            C                       G
+    Slowin' down to take a look at me
+    
+    [Chorus]
+            Em D             C  G
+    Come on, ba-by, don't say maybe
+            Am                 C              Em
+    I gotta know if your sweet love is gonna save me
+        C               G                  C             G
+    We may lose and we may win, though we may never be here again
+            Am               C               G
+    So open up, I'm climbin' in, so take it easy
+    
+    [Solo]
+    | G   | G   | G D | C   |
+    | G   | D   | C   | G   |
+    | Em  | D   | C   | G   |
+    | Am  | C   | Em  | Em D
+    
+    [Verse 3]
+                G
+    Well, I'm a-runnin' down the road tryin' to loosen my load
+                        D     Am
+    Got a world of trouble on my mind
+    G                       D                          C           G
+    Lookin' for a lover who won't blow my cover, she's so hard to find
+    
+    [Chorus]
+            Em            C G
+    Take it easy, take it easy
+                Am                C                Em
+    Don't let the sound of your own wheels make you crazy
+            C G             C  G
+    Come on baby, don't say maybe
+            Am                 C             G
+    I gotta know if your sweet love is gonna save me
+    
+    [Outro]
+    C
+    Oh oh oh, oh oh oh
+    G
+    Oh oh oh, oh oh oh
+    C
+    Oh oh oh, oh oh oh
+    G
+    Oh oh oh, oh oh oh
+    C
+    Oh oh oh, oh oh oh
+                G   G7   C
+    Oh, we got it e - e - asy
+                    G   G7   C
+    We oughta take it e - e - asy
+    
+    [Ending]
+    Em
+      `}</pre>
+
+        )
+          
+    
+  }
