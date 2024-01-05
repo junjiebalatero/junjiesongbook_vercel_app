d3cd8574e2706067445caeb22965435649ee21e2
junjiebalatero <junjiebalatero@gmail.com>
2024-01-05 14:42:31 +0800

    ok

---
+++ app/thegirlfromipanema/page.tsx
@@ -0,0 +1,96 @@
+import Link from 'next/link'
+export default function Page() {
+    return (
+        
+        // <Link href="/">this is dashboard click to go to home </Link>
+
+    <pre>{`
+    The Girl From Ipanema
+        
+    [Verse 1]
+    Fmaj7
+    Tall and tanned and young and lovely
+        G7
+    The girl from ipanema goes walking
+        Gm7                   F#7b5                   Fmaj7  F#7b5
+    And when she passes, each one she passes goes - ahhh!
+    
+    Fmaj7
+    When she walks, shes like a samba
+        G7
+    That swings so cool and sways so gentle
+        Gm7              F#7b5                        Fmaj7
+    That when she passes, each one she passes goes - oohh!
+    
+    
+    [Chorus]
+    F#maj7                  B9
+    Oh, but I watch her so sadly,
+    F#m7                  D9
+    How can I tell her I love her?
+    Gm7                         Eb9
+    Yes, I would give my heart gladly
+            Am7                        Abm7
+    But each day, when she walks to the sea
+        Gm7                         F#7b5
+    She looks straight ahead, not at me
+    
+    
+    [Verse 2]
+    Fmaj7
+    Tall, and tanned and young and lovely
+        G7
+    The girl from Ipanema goes walking
+        Gm7                 F#7b5                   Fmaj7
+    And when she passes, I smile - but she doesnt see
+    F#7b5              Fmaj7 F#7b5                 Fmaj7      F#7b5  Fmaj7
+    She just doesn't see, no, she just doesn't see...
+    
+    
+    [Verse 3]
+    Fmaj7                                        G7
+    Olha, que coisa mais linda, Mais cheia de graça,
+    G7                              Gm7
+    É ela, menina, que vem e que passa
+    Gm7          F#7b5           Fmaj7  F#7b5
+    Num doce balanço, a caminho do mar
+    Fmaj7                             G7
+    Moça do corpo dourado, do sol de ipanema,
+    G7                             Gm7
+    O seu balançado mais que um poema
+        F#7b5                     Fmaj7
+    a coisa mais linda que eu já vi passar
+    
+    
+    [Solo]
+    Fmaj7 G7 Gm7 F#7b5 Fmaj7 F#7b5
+    
+    
+    [Chorus]
+    F#maj7                  B9
+    Oh, but I watch her so sadly,
+    F#m7                  D9
+    How can I tell her I love her?
+    Gm7                         Eb9
+    Yes, I would give my heart gladly
+            Am7                        Abm7
+    But each day, when she walks to the sea
+        Gm7                         F#7b5
+    She looks straight ahead, not at me
+    
+    
+    [Verse 4]
+    Fmaj7
+    Tall, and tanned and young and lovely
+        G7
+    The girl from Ipanema goes walking
+        Gm7                 F#7b5                   Fmaj7
+    And when she passes, I smile - but she doesnt see 
+    F#7b5              Fmaj7 F#7b5                 Fmaj7      F#7b5  Fmaj7
+    She just doesn't see, no, she just doesn't see 
+       `}</pre>
+
+        )
+          
+    
+  }
