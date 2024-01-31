3b8192ae8fd735580ae9609b5ce31de5ed8e9b00
junjiebalatero <junjiebalatero@gmail.com>
2024-01-31 21:59:12 +0800

    ok

---
+++ app/whilemyguitargentlyweep/page.tsx
@@ -0,0 +1,79 @@
+import Link from 'next/link'
+export default function Page() {
+    return (
+        
+        // <Link href="/">this is dashboard click to go to home </Link>
+
+    <pre>{`
+            
+    [Intro]
+    Am Am/G F#m7b5 Fmaj7
+    Am G D E
+    
+    [Verse 1]
+    Am          Am/G         F#m7b5            Fmaj7
+    I look at you all, see the love there that's sleeping
+    Am            G          D Dsus4 D Dsus2 D Esus4 E
+    While my guitar gently weeps
+    Am          Am/G         F#m7b5       Fmaj7
+    I look at the floor, and I see it needs sweeping
+    Am            G          C     E
+    Still my guitar gently weeps
+    
+    [Chorus 1]
+    A              C#m  F#m        C#m
+    I don't know why      nobody told you
+    Bm                   E Esus4 E
+    How to unfold your love
+    A              C#m  F#m            C#m
+    I don't know how      someone controlled you
+    Bm                     E Esus4 E
+    They bought and sold you
+    
+    [Verse 2]
+    Am          Am/G         F#m7b5      Fmaj7
+    I look at the world, and I notice it's turning
+    Am            G          D Dsus4 D Dsus2 D Esus4 E
+    While my guitar gently weeps
+        Am       Am/G         F#m7b5    Fmaj7
+    With every mistake we must surely be learning
+    Am            G          C     E
+    Still my guitar gently weeps
+    
+    [Intrumental]
+    Am Am/G F#m7b5 Fmaj7
+    Am G D E
+    Am Am/G F#m7b5 Fmaj7
+    Am G C E
+    
+    [Chorus 2]
+    A              C#m  F#m            C#m
+    I don't know how      you were diverted
+    Bm                   E Esus4 E
+    You were perverted too
+    A              C#m  F#m            C#m
+    I don't know how      you were inverted
+    Bm               E Esus4 E
+    No one alerted you
+    
+    [Verse 3]
+    Am          Am/G         F#m7b5            Fmaj7
+    I look at you all, see the love there that's sleeping
+    Am            G          D Dsus4 D Dsus2 D Esus4 E
+    While my guitar gently weeps
+    Am          Am/G       F#m7b5       Fmaj7
+    Look at you all...
+    Am            G          C     E
+    Still my guitar gently weeps
+    
+    [Outro]
+    Am Am/G F#m7b5 Fmaj7
+    Am G D E
+    Am Am/G F#m7b5 Fmaj7
+    Am G C E
+       `}</pre>
+
+        )
+          
+    
+  }
